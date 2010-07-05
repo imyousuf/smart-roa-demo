1346aef53228c306adf54cafcc2c0dc365f11bd0
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-07-05 17:02:07 +0600

    Add basic project structure to demonstrate ROA
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

---
+++ src/main/java/com/smartitengineering/demo/roa/RootResource.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.smartitengineering.demo.roa;
+
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author imyousuf
+ */
+@Path("/")
+public class RootResource {
+
+}
