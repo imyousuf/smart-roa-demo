7a9a7e88be8b7f36d5a3aa6f3018a1bc12d9121a
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-07-06 19:07:57 +0600

    Add author content GET
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- src/main/java/com/smartitengineering/demo/roa/AuthorResource.java
+++ src/main/java/com/smartitengineering/demo/roa/AuthorResource.java
@@ -6,15 +6,19 @@ package com.smartitengineering.demo.roa;
 
 import com.smartitengineering.demo.roa.domains.Author;
 import com.smartitengineering.demo.roa.services.Services;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 
 /**
  *
  * @author imyousuf
  */
-@Path("/authors/{authorId}")
+@Path("/authors/id/{authorId}")
 public class AuthorResource extends AbstractResource {
 
   static final UriBuilder AUTHOR_URI_BUILDER = UriBuilder.fromResource(AuthorResource.class);
@@ -23,7 +27,7 @@ public class AuthorResource extends AbstractResource {
   static {
     AUTHOR_CONTENT_URI_BUILDER = AUTHOR_URI_BUILDER.clone();
     try {
-      AUTHOR_CONTENT_URI_BUILDER.path(BookResource.class.getMethod("getAuthor"));
+      AUTHOR_CONTENT_URI_BUILDER.path(AuthorResource.class.getMethod("getAuthor"));
     }
     catch (Exception ex) {
       throw new InstantiationError();
@@ -33,5 +37,12 @@ public class AuthorResource extends AbstractResource {
 
   public AuthorResource(@PathParam("authorId") Integer authorId) {
     author = Services.getInstance().getAuthorService().getById(authorId);
+  }
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Path("/content")
+  public Response getAuthor() {
+    return Response.ok(author).build();
   }
 }
